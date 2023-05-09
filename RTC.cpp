986d395363d9e93f71e1dc178ebb4374967194a6
Radu Iulian <iulianradu21@outlook.com>
2023-05-09 10:26:11 +0300

    Hours were reading wrong

--- RTC.cpp
+++ RTC.cpp
@@ -33,7 +33,7 @@ namespace iu
 
             date.second = ((date.second & 0b01110000) >> 4) * 10 + (date.second & 0b00001111);
             date.minute = ((date.minute & 0b01110000) >> 4) * 10 + (date.minute & 0b00001111);
-            date.hour   = ((date.hour   & 0b00110000) >> 4) * 10 + (date.hour   & 0b00000111); 
+            date.hour   = ((date.hour   & 0b00110000) >> 4) * 10 + (date.hour   & 0b00001111); 
             date.day    = ((date.day    & 0b00110000) >> 4) * 10 + (date.day    & 0b00001111);
             date.dotw   = (date.dotw    & 0b00000011);
             date.month  = ((date.month  & 0b00010000) >> 4) * 10 + (date.month  & 0b00001111);
